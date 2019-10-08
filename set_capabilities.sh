593d1230acb9265fbe817a9066a6fe658b55b1b8
Harald Jensås <hjensas@redhat.com>
2019-10-08 03:07:57 +0200

    Add set capabilities for 1x control 1x compute IPv6

--- set_capabilities.sh
+++ set_capabilities.sh
@@ -3,37 +3,24 @@ source /home/stack/stackrc
 openstack flavor create \
   --disk 40 --public --ram 4096 --vcpus 1 --rxtx-factor 1.0 \
   --property capabilities:boot_option='local' \
-  --property capabilities:profile='control-leaf1' \
+  --property capabilities:profile='control' \
   --property resources:CUSTOM_BAREMETAL='1' \
   --property resources:DISK_GB='0' \
   --property resources:MEMORY_MB='0' \
-  --property resources:VCPU='0' control-leaf1
+  --property resources:VCPU='0' control
 
 openstack flavor create \
   --disk 40 --public --ram 4096 --vcpus 1 --rxtx-factor 1.0 \
   --property capabilities:boot_option='local' \
-  --property capabilities:profile='compute-leaf1' \
+  --property capabilities:profile='compute' \
   --property resources:CUSTOM_BAREMETAL='1' \
   --property resources:DISK_GB='0' \
   --property resources:MEMORY_MB='0' \
-  --property resources:VCPU='0' compute-leaf1
-
-openstack flavor create \
-  --disk 40 --public --ram 4096 --vcpus 1 --rxtx-factor 1.0 \
-  --property capabilities:boot_option='local' \
-  --property capabilities:profile='compute-leaf2' \
-  --property resources:CUSTOM_BAREMETAL='1' \
-  --property resources:DISK_GB='0' \
-  --property resources:MEMORY_MB='0' \
-  --property resources:VCPU='0' compute-leaf2
+  --property resources:VCPU='0' compute
 
 openstack baremetal node set \
-  --property capabilities='profile:control-leaf1,boot_option:local' baremetal-leaf1-0
+  --property capabilities='profile:control,boot_option:local' baremetal-0
 openstack baremetal node set \
- --property capabilities='profile:compute-leaf1,boot_option:local' baremetal-leaf1-1
+ --property capabilities='profile:compute,boot_option:local' baremetal-1
 
-openstack baremetal node set \
-  --property capabilities='profile:compute-leaf2,boot_option:local' baremetal-leaf2-0
-openstack baremetal node set \
-  --property capabilities='profile:compute-leaf2,boot_option:local' baremetal-leaf2-1
 
