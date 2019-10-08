38d57efdf6c63ca330616bead3fefb43614d99c1
Harald Jensås <hjensas@redhat.com>
2019-10-08 03:05:14 +0200

    Add overcloud deploy environment for 1x contr 1x compute

---
+++ overcloud/deploy_overcloud.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source /home/centos/stackrc
+cd /home/centos
+
+openstack overcloud deploy \
+  --templates /home/centos/tripleo-heat-templates \
+  -r /home/centos/overcloud/ipv6_roles_data.yaml \
+  -n /home/centos/overcloud/ipv6_network_data.yaml \
+  -e /home/centos/tripleo-heat-templates/environments/enable-swap.yaml \
+  -e /home/centos/overcloud/environment/node_data.yaml \
+  -e /home/centos/tripleo-heat-templates/environments/network-isolation.yaml \
+  -e /home/centos/tripleo-heat-templates/environments/network-environment.yaml \
+  -e /home/centos/tripleo-heat-templates/environments/net-multiple-nics.yaml \
+  -e /home/centos/overcloud/environments/network-environment-overrides.yaml 
