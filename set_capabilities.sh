c29786472bd4984bcbff4e93a987f5c86cb4875e
Harald Jensås <hjensas@redhat.com>
2019-10-08 03:11:15 +0200

    Don't create flavors, use the defaults

--- set_capabilities.sh
+++ set_capabilities.sh
@@ -1,23 +1,5 @@
 source /home/centos/stackrc
 
-openstack flavor create \
-  --disk 40 --public --ram 4096 --vcpus 1 --rxtx-factor 1.0 \
-  --property capabilities:boot_option='local' \
-  --property capabilities:profile='control' \
-  --property resources:CUSTOM_BAREMETAL='1' \
-  --property resources:DISK_GB='0' \
-  --property resources:MEMORY_MB='0' \
-  --property resources:VCPU='0' control
-
-openstack flavor create \
-  --disk 40 --public --ram 4096 --vcpus 1 --rxtx-factor 1.0 \
-  --property capabilities:boot_option='local' \
-  --property capabilities:profile='compute' \
-  --property resources:CUSTOM_BAREMETAL='1' \
-  --property resources:DISK_GB='0' \
-  --property resources:MEMORY_MB='0' \
-  --property resources:VCPU='0' compute
-
 openstack baremetal node set \
   --property capabilities='profile:control,boot_option:local' baremetal-0
 openstack baremetal node set \
