07b14fa741ee5041b04796fce66b99ca1d498e90
Harald Jensås <hjensas@redhat.com>
2019-10-08 03:14:00 +0200

    Fix deploy command script - correct directory

--- overcloud/deploy_overcloud.sh
+++ overcloud/deploy_overcloud.sh
@@ -7,9 +7,9 @@ openstack overcloud deploy \
   --templates /home/centos/tripleo-heat-templates \
   -r /home/centos/overcloud/ipv6_roles_data.yaml \
   -n /home/centos/overcloud/ipv6_network_data.yaml \
-  -e /home/centos/tripleo-heat-templates/environments/enable-swap.yaml \
+  -e /home/centos/tripleo-heat-templates/environment/enable-swap.yaml \
   -e /home/centos/overcloud/environment/node_data.yaml \
   -e /home/centos/tripleo-heat-templates/environments/network-isolation.yaml \
   -e /home/centos/tripleo-heat-templates/environments/network-environment.yaml \
   -e /home/centos/tripleo-heat-templates/environments/net-multiple-nics.yaml \
-  -e /home/centos/overcloud/environments/network-environment-overrides.yaml 
+  -e /home/centos/overcloud/environment/network-environment-overrides.yaml 
