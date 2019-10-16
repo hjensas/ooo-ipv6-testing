1c0d781c22633f10e7b858340aab88f024de2de0
Harald Jensås <hjensas@redhat.com>
2019-10-16 09:46:08 +0200

    Update patches -1

--- ipv6-patches.sh
+++ ipv6-patches.sh
@@ -33,15 +33,13 @@ cd /home/centos
 git clone https://opendev.org/openstack/tripleo-common.git
 cd /home/centos/tripleo-common
 # Get undercloud name in get_undercloud_registry()
-git fetch https://review.opendev.org/openstack/tripleo-common refs/changes/05/687305/9 && git checkout FETCH_HEAD
-git checkout -b bug/1847141
-git rebase master
-git checkout master tripleo_common/tests
-git checkout master releasenotes
-git add tripleo_common
-git add releasenotes
-git commit -a --amend -m 'ready for patch'
-git format-patch master --stdout | sudo patch -p1 -d /usr/lib/python2.7/site-packages
-
+# git rebase master
+# git checkout master tripleo_common/tests
+# git checkout master releasenotes
+# git add tripleo_common
+# git add releasenotes
+# git commit -a --amend -m 'ready for patch'
+# git format-patch master --stdout | sudo patch -p1 -d /usr/lib/python2.7/site-packages
+# 
 cd /home/centos
 
