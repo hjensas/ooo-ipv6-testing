d2b166c149db68510fcffdc42fb0feacaf8f0f7b
Harald Jensås <hjensas@redhat.com>
2019-10-24 09:22:30 +0200

    clean up ipv6-patches

--- ipv6-patches.sh
+++ ipv6-patches.sh
@@ -29,22 +29,5 @@
 # 
 # cd /home/centos
 
-#
-# Patch cloud-init in built image
-#
-cd /home/centos
-
-git clone -b bug/1806014 https://git.launchpad.net/~harald-jensas/cloud-init
-
-LIBGUESTFS_BACKEND=direct virt-customize --add /home/centos/images/overcloud-full.qcow2 \
-  --delete /usr/lib/python2.7/site-packages/cloudinit \
-  --selinux-relabel
-LIBGUESTFS_BACKEND=direct virt-customize --add /home/centos/images/overcloud-full.qcow2 \
-  --copy-in /home/centos/cloud-init/cloudinit:/usr/lib/python2.7/site-packages/ \
-  --selinux-relabel
-LIBGUESTFS_BACKEND=direct virt-customize --add /home/centos/images/overcloud-full.qcow2 \
-  --delete /usr/lib/python2.7/site-packages/cloudinit/tests \
-  --selinux-relabel
-
 echo "DONE patching"
 
