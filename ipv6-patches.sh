59a5ab76649605efd5b41c4ed21bc9e63a916c35
Harald Jensås <hjensas@redhat.com>
2019-10-25 15:40:16 +0200

    Patch tripleoclient + fix radvd address to match gateway

--- ipv6-patches.sh
+++ ipv6-patches.sh
@@ -8,9 +8,16 @@
 #
 # python-tripleoclient changes
 #
-# cd /home/centos
-# git clone https://opendev.org/openstack/python-tripleoclient.git
-# cd /home/centos/python-tripleoclient
+cd /home/centos
+git clone https://opendev.org/openstack/python-tripleoclient.git
+cd /home/centos/python-tripleoclient
+git fetch https://review.opendev.org/openstack/python-tripleoclient refs/changes/05/690805/4 && git checkout FETCH_HEAD
+git checkout -b bug/1849590
+git checkout master tripleoclient/tests
+git add tripleoclient/tests
+git commit -a --amend -m 'ready for patch'
+git format-patch master --stdout | sudo patch -p1 -d /usr/lib/python2.7/site-packages
+
 
 #
 # tripleo-common changes
@@ -26,8 +33,8 @@
 # git add releasenotes
 # git commit -a --amend -m 'ready for patch'
 # git format-patch master --stdout | sudo patch -p1 -d /usr/lib/python2.7/site-packages
-# 
-# cd /home/centos
 
+
+cd /home/centos
 echo "DONE patching"
 
