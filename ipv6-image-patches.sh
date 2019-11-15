72eb80f22c680e9b1ea5c998d4be0a319f1a6031
Harald Jensås <hjensas@redhat.com>
2019-11-15 10:22:47 +0100

    Use github cloud-init -- they moved from LP to github

--- ipv6-image-patches.sh
+++ ipv6-image-patches.sh
@@ -7,7 +7,7 @@ set -e
 #
 cd /home/centos
 
-git clone -b bug/1806014 https://git.launchpad.net/~harald-jensas/cloud-init
+git clone -b bug/1806014 https://github.com/hjensas/cloud-init.git
 
 LIBGUESTFS_BACKEND=direct virt-customize --add /home/centos/images/overcloud-full.qcow2 \
   --delete /usr/lib/python2.7/site-packages/cloudinit \
