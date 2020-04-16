62d75c7457927092d21e0f009695d363251a26e2
Harald Jensås <hjensas@redhat.com>
2020-04-16 16:32:15 +0200

    Include container prep env in OC deploy command

--- overcloud/deploy_overcloud.sh
+++ overcloud/deploy_overcloud.sh
@@ -8,6 +8,7 @@ openstack overcloud deploy \
   --templates /home/centos/tripleo-heat-templates \
   -r /home/centos/overcloud/ipv6_roles_data.yaml \
   -n /home/centos/overcloud/ipv6_network_data.yaml \
+  -e /home/centos/containers-prepare-parameter.yaml \
   -e /home/centos/tripleo-heat-templates/environments/enable-swap.yaml \
   -e /home/centos/overcloud/environment/node_data.yaml \
   -e /home/centos/tripleo-heat-templates/environments/network-isolation.yaml \
